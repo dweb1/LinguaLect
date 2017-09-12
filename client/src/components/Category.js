30e03b5f79e22a064fe8154e0a77174a48b96e45
dweb1 <david.p.weber@gmail.com>
2017-09-12 18:23:03 -0400

    removed unnecessary imports from Category and Flashcard components

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -1,6 +1,5 @@
-import React, { Component } from "react";
-import axios from 'axios';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 import styled from 'styled-components'
 
 const CatCard = styled.div`
