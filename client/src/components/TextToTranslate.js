209ba7d847d98fb0cad4f0b6811c32758d74a822
dweb1 <david.p.weber@gmail.com>
2017-09-13 22:32:09 -0400

    Added styling to button on translate page

--- client/src/components/TextToTranslate.js
+++ client/src/components/TextToTranslate.js
@@ -1,6 +1,27 @@
 import React  from 'react';
 import axios from 'axios';
 import { parseString } from 'xml2js'
+import styled from 'styled-components'
+
+const Button = styled.div`
+    width: 30%;
+    text-decoration: none;
+    color: #355fa3;
+    text-align: center;
+    margin: 20px 0;
+    background-color: rgb(230, 242, 255);
+    border-radius: 4px;
+    :hover {
+        background-color: #355fa3;
+        a {
+            color: white
+        }
+    }
+    a {
+        text-decoration: none;
+        display: block
+    }
+`
 
 const TextToTranslate = (props) => {
 
@@ -14,7 +35,7 @@ const TextToTranslate = (props) => {
             <form method="get">
                 <label>Text to translate:</label> <br/>
                 <textarea id="textEntered" name="userText" />
-                <div onClick={handleClick}>Click here to translate</div>
+                <Button onClick={handleClick}>Click here to translate</Button>
             </form>
         </div>
     )
